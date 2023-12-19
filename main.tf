7f4c701bd398e473e94ef0aca645e9a16d3e2c0b
EC2 Default User <ec2-user@ip-172-31-43-254.ec2.internal>
2023-12-19 11:55:16 +0000

    should define your key file

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "amazon-linux-2" {
+data "aws_ami" "amazon-linux-2023" {
   owners      = ["amazon"]
   most_recent = true
 
@@ -24,24 +24,17 @@ data "aws_ami" "amazon-linux-2" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm*"]
-  }
-}
-
-data "template_file" "userdata" {
-  template = file("${abspath(path.module)}/userdata.sh")
-  vars = {
-    server-name = var.server-name
+    values = ["al2023-ami-2023*"]
   }
 }
 
 resource "aws_instance" "tfmyec2" {
-  ami = data.aws_ami.amazon-linux-2.id
-  instance_type = var.instance_type
-  count = var.num_of_instance
-  key_name = var.key_name
+  ami                    = data.aws_ami.amazon-linux-2023.id
+  instance_type          = var.instance_type
+  count                  = var.num_of_instance
+  key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
-  user_data = data.template_file.userdata.rendered
+  user_data              = templatefile("${abspath(path.module)}/userdata.sh", { myserver = var.server-name })
   tags = {
     Name = var.tag
   }
@@ -49,7 +42,6 @@ resource "aws_instance" "tfmyec2" {
 
 resource "aws_security_group" "tf-sec-gr" {
   name = "${var.tag}-terraform-sec-grp"
-
   tags = {
     Name = var.tag
   }
@@ -58,17 +50,17 @@ resource "aws_security_group" "tf-sec-gr" {
     for_each = var.docker-instance-ports
     iterator = port
     content {
-      from_port = port.value
-      to_port = port.value
-      protocol = "tcp"
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
 
   egress {
-    from_port =0
-    protocol = "-1"
-    to_port =0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
--- userdata.sh
+++ userdata.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-hostnamectl set-hostname ${server-name}
-yum update -y
-amazon-linux-extras install docker -y
+hostnamectl set-hostname ${myserver}
+dnf update -y
+dnf install docker -y
 systemctl start docker
 systemctl enable docker
 usermod -a -G docker ec2-user
 # install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" \
--o /usr/local/bin/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
